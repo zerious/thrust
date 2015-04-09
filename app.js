cce20226db357a9adf5567b8750743160b82f96b
Sam Eubank <sameubank+zerious@gmail.com>
2015-04-09 09:08:36 -0700

    v1.0.0

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ module.exports = lighter({
 
   port: 32123 || process.env.THRUST_PORT,
 
-  processCount: 2,
+  processCount: 1,
 
   dbs: {
     thrust: {
@@ -37,7 +37,7 @@ module.exports = lighter({
 
   rings: {
     thrust: {
-      hostPattern: '(bunny|sam).local',
+      //hostPattern: '(bunny|sam).local',
       replicas: 3
     }
   }
